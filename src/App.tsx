c213b03725fb4c7d917471df439d2b0533755bd0
SirMissir <pkv_96@icloud.com>
2022-10-16 22:13:42 +0300

    Lesson 7

--- src/App.tsx
+++ src/App.tsx
@@ -3,10 +3,32 @@ import './App.css';
 
 function App() {
   return (
-    <div className="App">
-     Hello, samurai! Let's go!
-    </div>
+      <div>
+          <Header />
+          <Technologies />
+      </div>
   );
+}
+function Technologies (){
+    return (
+        <div>
+            <ul>
+                <li>css</li>
+                <li>html</li>
+                <li>js</li>
+                <li>react</li>
+            </ul>
+        </div>
+    )
+}
+function Header() {
+    return (
+        <div>
+            <a href="">Home</a>
+            <a href="">News Feed</a>
+            <a href="">Messages</a>
+        </div>
+    );
 }
 
 export default App;
