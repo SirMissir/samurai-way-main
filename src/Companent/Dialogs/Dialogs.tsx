3d5414562318262f57a4a687a6a6deecdf24ab70
SirMissir <pkv_96@icloud.com>
2024-04-02 23:02:13 +0300

    feat: use of the `map` method to display items in the dialog list component

--- src/Companent/Dialogs/Dialogs.tsx
+++ src/Companent/Dialogs/Dialogs.tsx
@@ -28,28 +28,24 @@ const Dialogs = (props:any) => {
         {id:4, name:"Lera"},
         {id:5, name:"Victor"}
     ]
-    let messageData =[
+    let dialogsElements = dialogsData.map( dialogs=> <DialogItem name={dialogs.name} id={dialogs.id}/>)
+
+    let messagesData =[
         {id:1, name:"Hi"},
         {id:2, name:"How is your it-kamasutra"},
         {id:3, name:"OK"},
         {id:4, name:"How much your learning"},
         {id:5, name:"Victor"}
     ]
+    let messageElements = messagesData.map(messages => <Message message = {messages.name} id={messages.id}/>)
 
   return(
       <div className={s.dialogs}>
           <div className={s.dialogs_members}>
-              <DialogItem name={dialogsData[0].name} id={dialogsData[0].id}/>
-              <DialogItem name={dialogsData[1].name} id={dialogsData[1].id}/>
-              <DialogItem name={dialogsData[2].name} id={dialogsData[2].id}/>
+              {dialogsElements}
           </div>
           <div className={s.messages}>
-              <Message message = {messageData[0].name} id={messageData[0].id}/>
-              <Message message = {messageData[1].name} id={messageData[1].id}/>
-              <Message message = {messageData[2].name} id={messageData[2].id}/>
-              <Message message = {messageData[3].name} id={messageData[3].id}/>
-              <Message message = {messageData[4].name} id={messageData[4].id}/>
-
+              {messageElements}
           </div>
       </div>
   )
