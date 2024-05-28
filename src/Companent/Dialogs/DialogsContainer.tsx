77faf49bd282c4a22c9feac301e6e67aafd2bade
SirMissir <pkv_96@icloud.com>
2024-05-28 23:42:57 +0300

    refactor: react-redux for improved state management

--- src/Companent/Dialogs/DialogsContainer.tsx
+++ src/Companent/Dialogs/DialogsContainer.tsx
@@ -2,23 +2,23 @@ import React from "react";
 import {addMessageAC, newMessageTextAC} from "../../redux/dialogs-reducer";
 import {DialogsPageType} from "../../redux/store";
 import Dialogs from "./Dialogs";
-
+import {connect} from "react-redux";
 
 
 type PropsType = {
     state: DialogsPageType
-    dispatch: (action:any) => void
+    dispatch: (action: any) => void
 }
 
 
 const DialogsContainer = (props: PropsType) => {
 
-    let onAddMessage = (text:string) => {
+    let onAddMessage = (text: string) => {
         let action = addMessageAC(text)
         props.dispatch(action)
     }
 
-    let updateNewDialogText = (text:string) => {
+    let updateNewDialogText = (text: string) => {
         let action = newMessageTextAC(text)
         props.dispatch(action)
     }
@@ -31,6 +31,20 @@ const DialogsContainer = (props: PropsType) => {
         />
     )
 }
+
+let mapStateToProps = (state) => {
+    return {
+        dialogsPage: state.dialogsPage
+    }
+}
+let mapDispatchToProps = () => {
+    return {
+        updateNewDialogText: ()=>{},
+        onAddMessage: ()=>{}
+    }
+}
+
+const SuperDialogsContainer = connect(mapStateToProps , mapDispatchToProps)(Dialogs);
 
 export default DialogsContainer
 
--- src/index.tsx
+++ src/index.tsx
@@ -5,26 +5,26 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import {HashRouter} from "react-router-dom";
+import {Provider} from "react-redux";
 
 
-
-
-
-let rerenderEntireTree = (state:any) => {
+let rerenderEntireTree = (state: any) => {
     debugger
     ReactDOM.render(
         <HashRouter>
-            <App
-                state={state}
-                dispatch={store.dispatch.bind(store)}
-                store={store}
-            />
+            <Provider store={store}>
+                <App
+                    state={state}
+                    dispatch={store.dispatch.bind(store)}
+                    store={store}
+                />
+            </Provider>
         </HashRouter>,
         document.getElementById('root')
     );
 }
 rerenderEntireTree(store.getState());
-store.subscribe(()=>{
+store.subscribe(() => {
     let state = store.getState();
     rerenderEntireTree(state)
 });
