a68892a16db57d28ebcd7db6ba2a92ebaa7e0ca1
SirMissir <pkv_96@icloud.com>
2024-06-01 23:30:53 +0300

    fix: Container Component

--- src/Companent/Profile/MyPosts/MyPostsContainer.tsx
+++ src/Companent/Profile/MyPosts/MyPostsContainer.tsx
@@ -2,7 +2,6 @@ import React from 'react';
 import { PostType} from "../../../redux/store";
 import {addPostAC, newPostTextAC, profileReducerType} from "../../../redux/profile-reducer";
 import MyPosts from "./MyPosts";
-import {text} from "stream/consumers";
 import {connect} from "react-redux";
 import {Dispatch} from "redux";
 
@@ -20,42 +19,40 @@ type MapDispatchToPropsType = {
     updateNewPostText: (text: string) => void
 }
 
-function MyPostsContainer(props: ProfileType) {
-    // let state = props.store.getState() // L42 реализованно на уровне App, нет смысла на столько глубоко прокидывать данные
+// function MyPostsContainer(props: ProfileType) {
+//     // let state = props.store.getState() // L42 реализованно на уровне App, нет смысла на столько глубоко прокидывать данные
+//
+//     let addPost = (text:string) => {
+//         let action = addPostAC(text)
+//         // props.store.dispatch(action); //L42 можно добавить store для большего функционала( но загрязняет данными)
+//         props.dispatch(action);
+//     }
+//     let onPostChange = (text:string) => {
+//         let action = newPostTextAC(text)
+//         props.dispatch(action)
+//     }
+//
+//     return (<MyPosts
+//         posts={props.posts}
+//         newPostText={props.newPostText}
+//         updateNewPostText={onPostChange}
+//         addPost={addPost}
+//     />)
+// }
 
-    let addPost = (text:string) => {
-        let action = addPostAC(text)
-        // props.store.dispatch(action); //L42 можно добавить store для большего функционала( но загрязняет данными)
-        props.dispatch(action);
-    }
-    let onPostChange = (text:string) => {
-        let action = newPostTextAC(text)
-        props.dispatch(action)
-    }
-
-    return (<MyPosts
-        posts={props.posts}
-        newPostText={props.newPostText}
-        updateNewPostText={onPostChange}
-        addPost={addPost}
-    />)
-}
-
-const mapStateToProps = (state)=>{
+const mapStateToProps = (state:any)=>{
     return{
         posts:state.profilePage.posts,
         newPostText:state.profilePage.newPostText
     }
 }
-const mapDispatchToProps (dispatch:Dispatch):MapDispatchToPropsType=>{
+const mapDispatchToProps = (dispatch:Dispatch):MapDispatchToPropsType=>{
     return {
         updateNewPostext: (text:string) =>{
-            let action = newPostTextAC(text)
-            props.dispatch(action)
+            dispatch(newPostTextAC(text));
         },
-        addPost:()=>{
-            let action = addPostAC(text)
-            props.dispatch(action);
+        addPost:(text:string)=>{
+            dispatch(addPostAC(text));
         }
     }
 }
