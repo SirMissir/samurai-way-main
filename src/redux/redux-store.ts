6bab9f995e5a1875ffa52996a5ddf465006786b9
SirMissir <pkv_96@icloud.com>
2024-06-25 23:03:27 +0300

    refactor:  add ActionsTypes,StoreType

--- src/redux/redux-store.ts
+++ src/redux/redux-store.ts
@@ -1,7 +1,15 @@
-import profileReducer from "./profile-reducer";
-import dialogsReducer from "./dialogs-reducer";
+import profileReducer, {addPostAC, newPostTextAC} from "./profile-reducer";
+import dialogsReducer, {addMessageAC, newMessageTextAC} from "./dialogs-reducer";
 import sidebarReducer from "./sidebar-reducer";
-import {combineReducers,legacy_createStore as createStore} from 'redux'
+import {combineReducers, legacy_createStore as createStore, Store} from 'redux'
+
+
+export type ActionsTypes = ReturnType<typeof addMessageAC>
+    |ReturnType<typeof newMessageTextAC>
+    |ReturnType<typeof addPostAC>
+    |ReturnType<typeof newPostTextAC>
+
+
 
 const rootReducer = combineReducers({
     profilePage: profileReducer,
@@ -9,12 +17,13 @@ const rootReducer = combineReducers({
     sidebarPage: sidebarReducer
 })
 
+export type RootReducerType = typeof rootReducer
+export type AppStateType = ReturnType<RootReducerType>
+export type StoreType = Store<AppStateType,ActionsTypes>
+
+export const store: StoreType  = createStore(rootReducer)
 
 
-
-export const store = createStore(rootReducer)
-
-export type AppRootStoreType = ReturnType<typeof rootReducer>
 
 
 // а это, чтобы можно было в консоли браузера обращаться к store в любой момент
