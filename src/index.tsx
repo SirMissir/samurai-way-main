1bc4c5c387f6b2a9b386bb0a0d8f594ce4561c29
SirMissir <pkv_96@icloud.com>
2024-06-26 21:48:09 +0300

    feat: use react-redux to improve state management

--- src/index.tsx
+++ src/index.tsx
@@ -1,28 +1,24 @@
-
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import {HashRouter} from "react-router-dom";
 import {Provider} from "react-redux";
-import store from "./redux/redux-store";
+import store, { StoreType} from "./redux/redux-store";
 
-let rerenderEntireTree = (state: StoreType) => {
+
+
+
+let rerenderEntireTree = () => {
     ReactDOM.render(
         <HashRouter>
             <Provider store={store}>
-                <App
-                    // state={state}
-                    // dispatch={store.dispatch.bind(store)}
-                    // store={store}
-                />
+                <App/>
             </Provider>
         </HashRouter>,
         document.getElementById('root')
     );
 }
-rerenderEntireTree(store.getState());
-store.subscribe(() => {
-    let state = store.getState();
-    rerenderEntireTree(state)
-});
+
+rerenderEntireTree();
+store.subscribe(() => rerenderEntireTree());
