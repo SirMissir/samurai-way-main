a033fef1bcede62bf7056ccde12a00e5a4b5e0ee
SirMissir <pkv_96@icloud.com>
2024-06-28 23:44:15 +0300

    refactor: add base state Users

---
+++ src/redux/users-reducer.ts
@@ -0,0 +1,52 @@
+
+export type UsersType = {
+    id: number
+    fullName: string
+    status: string
+    location:LocationType[]
+};
+export type LocationType = {
+    city:number
+    country:string
+};
+
+export type InitialUsersStateType = {
+    users: UsersType[]
+
+};
+
+
+let initialState:InitialUsersStateType = {
+    users:[
+        {id:1, fullName:'Dmitry',status:'i am student', location: { city: 'Minsk', country: ' Belarus'}},
+        {id:2, fullName:'Sasha',status:'i am student', location: { city: 'Minsk', country: ' Belarus'}},
+        {id:3, fullName:'Andrey',status:'i am student', location: { city: 'Minsk', country: ' Belarus'}},
+    ],
+}
+
+const usersReducer = (state:InitialUsersStateType=initialState, action:###):InitialUsersStateType => {
+    debugger
+    switch (action.type) {
+        case:'###'
+            return {};
+        case '###':
+            return {};
+        default:
+            return state;
+    }
+}
+
+export type addPostACType = ReturnType<typeof ###>;
+export const ### = () => {
+    return {
+
+    } as const;
+};
+
+export type newPostTextACType = ReturnType<typeof ###>;
+export const ### = () => {
+    return {
+
+    } as const;
+};
+export default usersReducer;
