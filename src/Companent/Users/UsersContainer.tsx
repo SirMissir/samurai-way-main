d353ebf5fadefc30005c7eba82d088374e3fafe7
SirMissir <pkv_96@icloud.com>
2024-07-04 09:44:54 +0300

    refactor: typed page logic UserContainer

--- src/Companent/Users/UsersContainer.tsx
+++ src/Companent/Users/UsersContainer.tsx
@@ -2,25 +2,31 @@ import React from 'react';
 import {connect} from "react-redux";
 import Users from "./Users";
 import {Dispatch} from "redux";
-import {followAC, setUsersAC, unfollowAC, UsersType} from "../../redux/users-reducer";
+import {followAC, InitialUsersStateType, setUsersAC, unfollowAC, UsersType} from "../../redux/users-reducer";
 import {AppStateType} from "../../redux/redux-store";
 
-let mapStateToProps = (state:AppStateType) => {
+export type UsersPropsType = MapStateToPropsType & MapDispatchToPropsType
+
+type MapStateToPropsType = {
+    usersPage: InitialUsersStateType
+}
+
+type MapDispatchToPropsType = {
+    follow: (userId: number) => void
+    unfollow: (userId: number) => void
+    setUsers: (users: UsersType[]) => void
+}
+
+let mapStateToProps = (state:AppStateType):MapStateToPropsType => {
     return {
-        users: state.usersPage.users
+        usersPage: state.usersPage
     }
 };
-let mapDispatchToProps = (dispatch:Dispatch) => {
+let mapDispatchToProps = (dispatch:Dispatch):MapDispatchToPropsType => {
     return {
-        follow: (userId:number) => {
-            dispatch(followAC(userId))
-        },
-        unfollow: (userId:number) => {
-            dispatch(unfollowAC(userId))
-        },
-        setUsers: (users:UsersType[]) => {
-            dispatch(setUsersAC(users))
-        }
+        follow  : (userId:number)     => dispatch(followAC(userId)),
+        unfollow: (userId:number)     => dispatch(unfollowAC(userId)),
+        setUsers: (users:UsersType[]) => dispatch(setUsersAC(users))
     }
 };
 
