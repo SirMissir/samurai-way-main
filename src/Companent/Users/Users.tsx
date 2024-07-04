9727f5aaa96fad7395cbd5dd0eed402b01564bca
SirMissir <pkv_96@icloud.com>
2024-07-04 10:14:10 +0300

    refactor: add logic Follow/unFollow Users

--- src/Companent/Users/Users.tsx
+++ src/Companent/Users/Users.tsx
@@ -1,18 +1,50 @@
 import React from 'react';
+import {UsersPropsType} from "./UsersContainer";
 
 
 
 
-function Users() {
-    return (
-        <div >
+function Users(props:UsersPropsType) {
+    const { usersPage, follow, unfollow } = props;
+
+    const userItems = usersPage.users.map((user) => (
+        <div key={user.id}>
             <div>
-                Users
+                <img
+                    src={user.photoUrl}
+                    alt={user.fullName}
+                    width={50}
+                    height={50}
+                />
+                {
+                    user.followed ? (
+                        <button onClick={() => unfollow(user.id)}>
+                            Follow
+                        </button>
+                    ) : (
+                        <button onClick={() => follow(user.id)}>
+                            Unfollow
+                        </button>
+                    )
+                }
             </div>
-
-
+            <div>
+                <div>
+                    <h3>{user.fullName}</h3>
+                    <p>{user.status}</p>
+                </div>
+                <div>
+                    {`${user.location.city}, ${user.location.country}`}
+                </div>
+            </div>
         </div>
-    )
-}
+    ));
+
+    return (
+        <div>
+            {userItems}
+        </div>
+    );
+};
 
 export default Users;
