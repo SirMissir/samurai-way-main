0d654ee7704c64fb968c0afacfedd5464c07835d
SirMissir <pkv_96@icloud.com>
2024-07-31 20:17:12 +0300

    fix:  Users component to use class syntax

--- src/Companent/Users/Users.tsx
+++ src/Companent/Users/Users.tsx
@@ -1,59 +1,51 @@
-import React from 'react';
+import React, {Component} from 'react';
 import {UsersPropsType} from "./UsersContainer";
 import axios from "axios";
 import userDefault from '../../Assets/Img/user.png'
 import styles from './User.module.css'
 
+class Users extends Component<UsersPropsType> {
+    constructor(props: UsersPropsType) {
+        super(props);
 
-function Users(props: UsersPropsType) {
-    const {usersPage, follow, unfollow, setUsers} = props;
-
-    if (!usersPage.users.length) {
         axios.get('https://social-network.samuraijs.com/api/1.0/users')
-            .then(response => {
-                debugger
-                setUsers(response.data.items)
-            })
-    }
+            .then((response) => this.props.setUsers(response.data.items));
+    };
 
-    const userItems = usersPage.users.map((user) => (
-        <div key={user.id} className={styles.userItem}>
-            <div className={styles.userSubsBlock}>
-                <img
-                    src={user.photos.large ? user.photos.large : userDefault}
-                    alt={user.name}
-                    width={50}
-                    height={50}
-                />
-                {
-                    user.followed ? (
-                        <button onClick={() => unfollow(user.id)}>
-                            Follow
-                        </button>
-                    ) : (
-                        <button onClick={() => follow(user.id)}>
-                            Unfollow
-                        </button>
-                    )
-                }
-            </div>
-            <div className={styles.userInfo}>
-                <div className={styles.userDetails}>
-                    <h3 className={styles.username}>{user.name}</h3>
-                    <p className={styles.userStatus}>{user.status}</p>
+    render = () => (
+
+        this.props.usersPage.users.map((user) => (
+            <div key={user.id} className={styles.userItem}>
+                <div className={styles.userSubsBlock}>
+                    <img
+                        src={user.photos.large ? user.photos.large : userDefault}
+                        alt={user.name}
+                        width={50}
+                        height={50}
+                    />
+                    {
+                        user.followed ? (
+                            <button onClick={() => this.props.unfollow(user.id)}>
+                                Follow
+                            </button>
+                        ) : (
+                            <button onClick={() => this.props.follow(user.id)}>
+                                Unfollow
+                            </button>
+                        )
+                    }
                 </div>
-                <div className={styles.userLocation}>
-                    {/*{`${user.location.city}, ${user.location.country}`}*/}
+                <div className={styles.userInfo}>
+                    <div className={styles.userDetails}>
+                        <h3 className={styles.username}>{user.name}</h3>
+                        <p className={styles.userStatus}>{user.status}</p>
+                    </div>
+                    <div className={styles.userLocation}>
+                        {/*{`${user.location.city}, ${user.location.country}`}*/}
+                    </div>
                 </div>
             </div>
-        </div>
-    ));
-
-    return (
-        <div className={styles.users}>
-            {userItems}
-        </div>
-    );
-};
-
+        ))
+    )
+}
 export default Users;
--- src/Companent/Users/UsersContainer.tsx
+++ src/Companent/Users/UsersContainer.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import {connect} from "react-redux";
-import Users from "./UsersС";
+import Users from "./Users";
 import {Dispatch} from "redux";
 import {followAC, InitialUsersStateType, setUsersAC, unfollowAC, UserType} from "../../redux/users-reducer";
 import {AppStateType} from "../../redux/redux-store";
